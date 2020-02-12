70721e79f276f22c8655b6c3db5ce7de5d6ebf5f
Paul Grokenberger <grokepau@googlemail.com>
2020-02-12 16:02:24 +0200

    controllers ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+puts 'Downloading Ingredients'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+data = JSON.parse(user_serialized)
+data['drinks'].each do |drink|
+  Ingredient.create(name: drink['strIngredient1'])
+end
+
+puts 'Ingredients Downloaded'
